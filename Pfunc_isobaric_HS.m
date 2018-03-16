68bcd39af96602701e522a06549d9a57b3d4d2d7
Yi Hu <aba@Aba-MacBook-Pro.local>
2018-03-16 00:02:00 -0400

    Release version

--- Pfunc_isobaric_HS.m
+++
@@ -1,6 +0,0 @@
-% Calculate the partition function, 1D hard sphere
-function pfunc = Pfunc_isobaric_HS(P, beta, coeffs, divide)
-    % set 2*pi*m/h^2 = 1
-    pfunc = exp(-beta*P*coeffs(1))./(beta*P);
-    pfunc = sqrt(1/beta)*pfunc;
-end
--- cuthe.m
+++
@@ -1,4 +0,0 @@
-% chop the first and the last element
-function result=cuthe(A)
-    result = A(2:end-1);
-end
